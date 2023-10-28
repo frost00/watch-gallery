505dbe244155bf0c46602ba3fb328f9a98a8a110
Shayon Maramkhah <frost00@github.com>
2023-10-28 17:08:00 -0400

    adding all files

---
+++ src/test/java/com/example/watchgallery/WatchgalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.watchgallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchgalleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
